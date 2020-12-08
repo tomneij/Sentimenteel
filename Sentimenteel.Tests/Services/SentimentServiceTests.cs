db6d94be1d0ed81c34230b1e5e0cf0b560b45cab
Thomas Bardoel <tbardoel@onvz.nl>
2020-12-08 18:22:32 +0100

    eerste opzetje Text Analytics API

--- Sentimenteel.Tests/Services/SentimentServiceTests.cs
+++ Sentimenteel.Tests/Services/SentimentServiceTests.cs
@@ -19,9 +19,31 @@ namespace Sentimenteel.Tests.Services
         [TestMethod]
         public void Analyze_PositiveText_SentimentPositive()
         {
-            string text = "ONVZ is echt fijn!";
-            Sentiment result = _service.Analyze(text);
+            //string text = "ONVZ is echt goed!";
+            string text = "ONVZ is very good!";
+
+            Sentiment result = _service.Analyze(text, "en", "us");
             result.Should().Be(Sentiment.Positive);
+        }
+
+        [TestMethod]
+        public void Analyze_NegativeText_SentimentNegative()
+        {
+            //string text = "ONVZ is echt slecht!";
+            string text = "ONVZ is very bad!";
+
+            Sentiment result = _service.Analyze(text, "en", "us");
+            result.Should().Be(Sentiment.Negative);
+        }
+
+        [TestMethod]
+        public void Analyze_NeutralText_SentimentNeutral()
+        {
+            //string text = "ONVZ is echt gewoon!";
+            string text = "ONVZ is very average!";
+
+            Sentiment result = _service.Analyze(text, "en", "us");
+            result.Should().Be(Sentiment.Neutral);
         }
     }
 }
