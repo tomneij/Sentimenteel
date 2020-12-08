9c299f49a5410a1bace2789356474ec7eb0a0fa1
Tom Neij <tneij@onvz.nl>
2020-12-08 14:37:37 +0100

    Eerste opzetje voor overzicht

---
+++ src/Website/Controllers/OverviewController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using Website.Factories;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class OverviewController : Controller
+    {
+
+        private string[] Labels = { "ONVZ", "Ohra", "CZ", "VGZ", "ZilverenKruis" };
+
+     
+        public ActionResult Index()
+        {
+            var overviewModel = new OverviewViewModel();
+            var overviewFactory = new OverviewFactory();
+           
+            foreach (var label in Labels)
+            {
+                var overviewItem = overviewFactory.GetLabelSentiment(label);
+                overviewModel.Items.Add(overviewItem);
+            }
+
+            return View(overviewModel);
+        }
+    }
+}
---
+++ src/Website/Factories/OverviewFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Website.Models;
+
+namespace Website.Factories
+{
+    public class OverviewFactory
+    {
+        public OverviewFactory ()
+        {
+
+        }
+
+        public OverviewItemViewModel GetLabelSentiment(string label)
+        {
+
+            var rnd = new Random();
+
+            return new OverviewItemViewModel
+            {
+                Name = label,
+                Icon = GetIcon(label),
+                Positive = rnd.Next(1,100),
+                Neutral = rnd.Next(1,100),
+                Negative = rnd.Next(1,100),
+            };
+        }
+
+        private string GetIcon(string label)
+        {
+            return $"/Content/Icons/{label}.png";
+        }
+    }
+}
