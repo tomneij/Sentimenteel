ed258a52220d56c4fec40ee0952f57df070f323d
Marnix de Vrije <MdVrije@onvz.nl>
2020-12-09 09:12:18 +0100

    Added simple charts setup

--- src/Website/Controllers/HomeController.cs
+++ src/Website/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using Website.Models;
 
 namespace Website.Controllers
 {
@@ -11,6 +10,65 @@ namespace Website.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Trend()
+        {
+            ViewBag.Message = "Trend";
+            var positive = this.SetPositiveDataPoints();
+            var neutral = this.SetNeutralDataPoints();
+            var negative = this.SetNegativeDataPoints();
+
+            ViewBag.Positive = JsonConvert.SerializeObject(positive);
+            ViewBag.Neutral = JsonConvert.SerializeObject(neutral);
+            ViewBag.Negative = JsonConvert.SerializeObject(negative);
+            return View();
+        }
+
+        private List<DataPoint> SetPositiveDataPoints()
+        {
+            var dataPoints = new List<DataPoint>
+            {
+                new DataPoint("Jan", 72),
+                new DataPoint("Feb", 67),
+                new DataPoint("Mar", 55),
+                new DataPoint("Apr", 42),
+                new DataPoint("May", 40),
+                new DataPoint("Jun", 35)
+            };
+
+            return dataPoints;
+        }
+
+
+        private object SetNeutralDataPoints()
+        {
+            var dataPoints = new List<DataPoint>
+            {
+                new DataPoint("Jan", 48),
+                new DataPoint("Feb", 56),
+                new DataPoint("Mar", 50),
+                new DataPoint("Apr", 47),
+                new DataPoint("May", 65),
+                new DataPoint("Jun", 69)
+            };
+
+            return dataPoints;
+        }
+
+        private object SetNegativeDataPoints()
+        {
+            var dataPoints = new List<DataPoint>
+            {
+                new DataPoint("Jan", 38),
+                new DataPoint("Feb", 46),
+                new DataPoint("Mar", 55),
+                new DataPoint("Apr", 70),
+                new DataPoint("May", 77),
+                new DataPoint("Jun", 91)
+            };
+
+            return dataPoints;
         }
 
         public ActionResult About()
